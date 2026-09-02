5d2f0ffda2f9be516e7f15c28de7ae3127b1ff52
Kateryna Biesiedovska <btv1812@gmail.com>
2026-09-02 16:38:54 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+APP_NAME = os.getenv('APP_NAME', 'python-backend')
+APP_VERSION = os.getenv('APP_VERSION', '1.0.0')
+APP_ENV = os.getenv('APP_ENV', 'production')
+PORT = int(os.getenv('PORT', 8080))
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({
+        'status': 'OK',
+        'message': "Python Backend is running",
+    }), 200
+
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({
+        'status': 'OK',
+        'message': "Python Backend is healthy",
+    }), 200
+
+
+@app.route('/info', methods=['GET'])
+def info():
+    return jsonify({
+        'app_name': APP_NAME,
+        'app_version': APP_VERSION,
+        'app_env': APP_ENV,
+        'port': PORT
+    }), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=PORT)
